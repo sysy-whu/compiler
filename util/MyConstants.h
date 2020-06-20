40674af8f347b7b714ea22249a78407bf2c639cd
lugf027 <1606962606@qq.com>
2020-06-20 21:37:00 +0800

    添加操作运算、数据类型相关常数

--- util/MyConstants.h
+++ util/MyConstants.h
@@ -52,11 +52,51 @@ static const std::string KEYWORD_BREAK = "break";
 static const std::string KEYWORD_CONTINUE = "continue";
 static const std::string KEYWORD_RETURN = "return";
 
-// some important char, for we'll be easy to find where such char has been used
+// some important char, it's mostly used in lexer for now.
+// and we'll be easy to find where such char has been used
 static const char CHAR_UNDERLINE = '_';
 static const char CHAR_SLASH = '/';
 static const char CHAR_STAR = '*';
 static const char CHAR_N_BACKEND_SLASH = '\n';
 static const char CHAR_R_BACKEND_SLASH = '\r';
+
+static const char CHAR_GREATER = '>';
+static const char CHAR_LOWER = '<';
+static const char CHAR_NOT = '!';
+static const char CHAR_AND = '&';
+static const char CHAR_OR = '|';
+static const char CHAR_EQ = '=';
+
+static const char CHAR_ADD = '+';
+static const char CHAR_SUB = '-';
+static const char CHAR_MUL = '*';
+static const char CHAR_DIV = '/';
+static const char CHAR_REM = '%';
+
+// some symbols for operation
+static const int OP_ASSIGN = -100;
+
+static const int OP_BO_ADD = -101;
+static const int OP_BO_SUB = -102;
+static const int OP_BO_MUL = -103;
+static const int OP_BO_DIV = -104;
+static const int OP_BO_REM = -105;
+
+static const int OP_BO_GT = -106;
+static const int OP_BO_LT = -107;
+static const int OP_BO_OR = -108;
+static const int OP_BO_EQ = -109;
+static const int OP_BO_AND = -110;
+static const int OP_BO_NEQ = -111;
+static const int OP_BO_GTE = -112;
+static const int OP_BO_LTE = -113;
+
+// data type
+static const std::string TYPE_INT_STR = "int";
+static const std::string TYPE_VOID_STR = "void";
+
+static const int TYPE_INT = -1;
+static const int TYPE_VOID = -2;
+static const int TYPE_INT_ARRAY = -3;
 
 #endif //COMPILER_MYCONSTANTS_H
