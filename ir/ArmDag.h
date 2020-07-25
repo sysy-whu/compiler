643501c4a77aad51e55fb4ff5bc3bcf483cd7d5e
zhangtao <773503141@qq.com>
2020-07-25 21:58:16 +0800

    armdag1.0

--- ir/ArmDag.h
+++ ir/ArmDag.h
@@ -10,194 +10,188 @@ class ArmDAGNode;
 
 class ArmDAGValue {
 private:
-  ArmDAGNode *Node = nullptr;
-  unsigned ResNo = 0;
+    ArmDAGNode *Node = nullptr;
+    unsigned ResNo = 0;
 public:
-  ArmDAGValue() = default;
+    ArmDAGValue() = default;
 
-  ArmDAGValue(ArmDAGNode *node, unsigned resno);
+    ArmDAGValue(ArmDAGNode *node, unsigned resno);
 
-  /// get the index which selects a specific result in the ArmDAGNode
-  unsigned getResNo() const { return ResNo; }
+    /// get the index which selects a specific result in the ArmDAGNode
+    unsigned getResNo() const { return ResNo; }
 
-  /// get the ArmDAGNode which holds the desired result
-  ArmDAGNode *getNode() const { return Node; }
+    /// get the ArmDAGNode which holds the desired result
+    ArmDAGNode *getNode() const { return Node; }
 
-  /// set the ArmDAGNode
-  void setNode(ArmDAGNode *N) { Node = N; }
+    /// set the ArmDAGNode
+    void setNode(ArmDAGNode *N) { Node = N; }
 
-  ArmDAGValue getValue(unsigned R) const {
-    return ArmDAGValue(Node, R);
-  }
+    ArmDAGValue getValue(unsigned R) const {
+        return ArmDAGValue(Node, R);
+    }
 };
 
 class ArmDAGUse {
 private:
-  /// Val - The value being used.
-  ArmDAGValue Val;
-  /// User - The user of this value.
-  ArmDAGNode *User = nullptr;
-  /// Prev, Next - Pointers to the uses list of the ArmDAGNode referred by
-  /// this operand.
-  ArmDAGUse **Prev = nullptr;
-  ArmDAGUse *Next = nullptr;
+    /// Val - The value being used.
+    ArmDAGValue Val;
+    /// User - The user of this value.
+    ArmDAGNode *User = nullptr;
+    /// Prev, Next - Pointers to the uses list of the ArmDAGNode referred by
+    /// this operand.
+    ArmDAGUse **Prev = nullptr;
+    ArmDAGUse *Next = nullptr;
 
 public:
-  ArmDAGUse() = default;
+    ArmDAGUse() = default;
 
-  ArmDAGUse(const ArmDAGValue &V, ArmDAGNode *p) {
-    setInitial(V);
-    setUser(p);
-  }
+    ArmDAGUse(const ArmDAGValue &V, ArmDAGNode *p) {
+        setInitial(V);
+        setUser(p);
+    }
 
-  /// If implicit conversion to ArmDAGValue doesn't work, the get() method returns
-  /// the ArmDAGValue.
-  const ArmDAGValue &get() const { return Val; }
+    /// If implicit conversion to ArmDAGValue doesn't work, the get() method returns
+    /// the ArmDAGValue.
+    const ArmDAGValue &get() const { return Val; }
 
-  /// This returns the ArmDAGNode that contains this Use.
-  ArmDAGNode *getUser() { return User; }
+    /// This returns the ArmDAGNode that contains this Use.
+    ArmDAGNode *getUser() { return User; }
 
-  /// Get the next ArmDAGUse in the use list.
-  ArmDAGUse *getNext() const { return Next; }
+    /// Get the next ArmDAGUse in the use list.
+    ArmDAGUse *getNext() const { return Next; }
 
-  /// Convenience function for get().getNode().
-  ArmDAGNode *getNode() const { return Val.getNode(); }
+    /// Convenience function for get().getNode().
+    ArmDAGNode *getNode() const { return Val.getNode(); }
 
-  /// Convenience function for get().getResNo().
-  unsigned getResNo() const { return Val.getResNo(); }
+    /// Convenience function for get().getResNo().
+    unsigned getResNo() const { return Val.getResNo(); }
 
 private:
-  friend class ArmDAGRoot;
+    friend class ArmDAGRoot;
 
-  friend class ArmDAGNode;
+    friend class ArmDAGNode;
 
-  void setUser(ArmDAGNode *p) { User = p; }
+    void setUser(ArmDAGNode *p) { User = p; }
 
-  inline void set(const ArmDAGValue &V);
+    inline void set(const ArmDAGValue &V);
 
-  inline void setInitial(const ArmDAGValue &V);
+    inline void setInitial(const ArmDAGValue &V);
 
-  inline void setNode(ArmDAGNode *N);
+    inline void setNode(ArmDAGNode *N);
 
-  void addToList(ArmDAGUse **List) {
-    Next = *List;
-    if (Next)
-      Next->Prev = &Next;
-    Prev = List;
-    *List = this;
-  }
+    void addToList(ArmDAGUse **List) {
+        Next = *List;
+        if (Next)
+            Next->Prev = &Next;
+        Prev = List;
+        *List = this;
+    }
 
-  void removeFromList() {
-    *Prev = Next;
-    if (Next)
-      Next->Prev = Prev;
-  }
+    void removeFromList() {
+        *Prev = Next;
+        if (Next)
+            Next->Prev = Prev;
+    }
 };
 
 class ArmDAGNode {
 private:
-  // 唯一标识
-  int id;
-  // 操作类型
-  ARM_DAG opType;
-  // 指定节点输出名称
-  std::string retName;
-  // 指定输入的ArmDAGUse
-  std::vector<ArmDAGUse *> OperandList;
-  // 用到自身的ArmDAGUse的头结点
-  ArmDAGUse *UseList = nullptr;
+    // 唯一标识
+    int id;
+    // 操作类型
+    ARM_DAG opType;
+
+    // arm指令操作数
+    std::string opNum1;
+    std::string opNum2;
+    std::string opNum3;
+
+    // 指定输入的ArmDAGUse
+    std::vector<ArmDAGUse *> dependUseList;
+    // 用到自身的ArmDAGUse的头结点
+    ArmDAGUse *UseList = nullptr;
 
 public:
-  ArmDAGNode(int id, ARM_DAG opType, const std::string retName) : id(id), opType(opType), retName(retName) {}
+    ArmDAGNode(int id, ARM_DAG opType) : id(id), opType(opType) {}
 
-  /// 这个方法只允许在ArmDAGUse中使用
-  void addUse(ArmDAGUse &U) { U.addToList(&UseList); }
+    ArmDAGNode(int id, ARM_DAG opType, std::string &opNum1) : id(id), opType(opType), opNum1(opNum1) {}
 
-  /**
-   * get函数
-   */
+    ArmDAGNode(int id, ARM_DAG opType, std::string &opNum1, std::string &opNum2) : id(id), opType(opType),
+                                                                                   opNum1(opNum1), opNum2(opNum2) {}
+
+    ArmDAGNode(int id, ARM_DAG opType, std::string &opNum1, std::string &opNum2, std::string &opNum3) : id(id),
+                                                                                                        opType(opType),
+                                                                                                        opNum1(opNum1),
+                                                                                                        opNum2(opNum2),
+                                                                                                        opNum3(opNum3) {}
+
+    /// armDagNode为依赖的node
+    void addDependUse(ArmDAGNode *armDagNode) {
+        ArmDAGValue armDagValue(armDagNode, 0);
+        ArmDAGUse *armDagUse = new ArmDAGUse(armDagValue, this);
+        dependUseList.emplace_back(armDagUse);
+    };
+    /// 这个方法只允许在ArmDAGUse中使用
+    void addUse(ArmDAGUse &U) { U.addToList(&UseList); }
+
+    /**
+     * get函数
+     */
 public:
-  ARM_DAG getOpType() const {
-    return opType;
-  }
+    ARM_DAG getOpType() const {
+        return opType;
+    }
 
-  const std::string &getRetName() const {
-    return retName;
-  }
 
-  std::vector<ArmDAGUse *> getOperandList() const {
-    return OperandList;
-  }
+    ArmDAGUse *getUseList() const {
+        return UseList;
+    }
 
-  ArmDAGUse *getUseList() const {
-    return UseList;
-  }
+    const std::string &getOpNum1() const {
+        return opNum1;
+    }
 
-  /**
-   * set函数
-   */
+    const std::string &getOpNum2() const {
+        return opNum2;
+    }
+
+    const std::string &getOpNum3() const {
+        return opNum3;
+    }
+
+    /**
+     * set函数
+     */
 public:
-  void setOpType(ARM_DAG opType) {
-    ArmDAGNode::opType = opType;
-  }
+    void setOpType(ARM_DAG opType) {
+        ArmDAGNode::opType = opType;
+    }
 
-  void setRetName(const std::string &retName) {
-    ArmDAGNode::retName = retName;
-  }
 
-  void setOperandList(std::vector<ArmDAGUse *> operandList) {
-    OperandList = operandList;
-  }
+    void setOpNum1(const std::string &opNum1) {
+        ArmDAGNode::opNum1 = opNum1;
+    }
+
+    void setOpNum2(const std::string &opNum2) {
+        ArmDAGNode::opNum2 = opNum2;
+    }
+
+    void setOpNum3(const std::string &opNum3) {
+        ArmDAGNode::opNum3 = opNum3;
+    }
 };
 
-class ImmediateArmDAGNode : public ArmDAGNode {
-private:
-  int value;
-
-public:
-  ImmediateArmDAGNode(int id, int value) : ArmDAGNode(
-          id, ARM_DAG::IMMEDIATE_DATA, "IMMEDIATE_DATA"),
-                                           value(value) {}
-};
 
 class ArmDAGRoot {
 private:
-  std::vector<ArmDAGNode *> nodes;
-  // 标记当前语句的数量
-  int count = 0;
-  // 已存在变量列表
-  std::vector<std::string> existVarList;
-
-private:
-  /// 对node进行深度遍历,返回一个ArmDAGNode，其retName与opd相等
-  ArmDAGNode *DepthArmDAGNode(ArmDAGNode *armDAGNode, const char *opd) {
-    for (auto &item : armDAGNode->getOperandList()) {
-      if (item->get().getNode()->getRetName() == opd) {
-        return item->get().getNode();
-      } else {
-        ArmDAGNode *node = DepthArmDAGNode(item->get().getNode(), opd);
-        if (node != nullptr)
-          return node;
-      }
-    }
-    return nullptr;
-  }
-
-  /// 寻找DAG中对应的指定targetRetName的node;
-  /// 然后将此node作为newNode的输入;
-  /// 如果newNodeRetName与targetRetName相同，则将指向此node的ArmDAGUse全部改为指向newNode;
-  void FindNode(ArmDAGNode *newNode, const char *newNodeRetName, const char *targetRetName);
-
-  /// 先判断opdName是否为临时变量
-  ///   是临时变量：
-  ///     不管existVarList存不存该变量在都返回true，不存在的话要将其添加到existVarList。
-  ///   不是临时变量
-  ///     existVarList存在该变量则直接返回true，不存在的话则为外部变量，先将其添加到existVarList，再返回false
-  bool AnalysisOpdName(const char *opdName);
+    std::vector<ArmDAGNode *> armDagNodes;
 
 public:
-  ArmDAGRoot() = default;
-  // ArmDAGRoot(std::vector<ArmDAGNode *> *nodes) : nodes(nodes){};
+    ArmDAGRoot() = default;
+
+    void addNode(ArmDAGNode *armDagNode) {
+        armDagNodes.emplace_back(armDagNode);
+    }
 
 };
 
